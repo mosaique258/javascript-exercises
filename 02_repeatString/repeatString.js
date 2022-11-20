b92d6d8703acaaec3a0f495c3926e7040fb9fba1
mosaique258 <mosaique258@gmail.com>
2022-11-20 08:44:57 +0100

    Complete second exercise repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(input, maxIteration) {
+    let output = '';
 
+    if (maxIteration < 0) return 'ERROR';
+    
+
+    for (let i = 0; i < maxIteration; i++) {
+        output = output.concat('',input);
+
+        
+    }
+    return output;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
