ac09e2fe6d81f7568453bcd0100cfc8ac6d4fa10
mosaique258 <mosaique258@gmail.com>
2022-11-20 08:56:13 +0100

    Complete third exercise reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    const myArray = input.split("");
+    const reverseArray = myArray.reverse();
+    const output = reverseArray.join('');
+    return output
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
