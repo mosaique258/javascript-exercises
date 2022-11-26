2cd69dbfc36a37053b1316e4b1c2a863f2143757
mosaique258 <mosaique258@gmail.com>
2022-11-26 08:13:32 +0100

    Complete exercise 5 - sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    let sum = 0;
+
+    if((!(typeof number1 == 'number')) || (!(typeof number2 == 'number'))) {
+        return 'ERROR'
+    }
+    
+    if ((number1 > 0) && (number2 > 0)) {
+        if (number2 > number1) {
+            for (let i = number1; i <= number2; i ++ ) {
+                sum += i
+                }
+            return sum;
+
+        } else {
+            for (let i = number2; i <= number1; i ++ ) {
+                sum += i
+                }
+                
+            return sum;
+        }
+    } else {
+        return 'ERROR'
+    
+    }        
 
 };
 
