c5a3450207874c14b4c792fc8003d596231482b4
mosaique258 <mosaique258@gmail.com>
2022-11-26 07:31:43 +0100

    Complete exercise 4 - removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,26 @@
-const removeFromArray = function() {
+// Create a function that takes random number of arguments (args)
+
+const removeFromArray = function(...args) {
+    
+         // Save first argument as an array
+        let inputArray = args[0];
+
+        // Create an ouput array (containing 0 elements at first)
+        let outputArray = [];
+
+        // Create a loop that iterates through elements of the input array...
+        for ( const i of inputArray ) {
+
+            //... and checks, if those elements are part of the arguments, if not...
+            if (!(args.includes(i,1))) {
+
+                //... add the element to the output array
+                outputArray.push(i);
+            }
+            
+        };
+        return outputArray; 
+    
 
 };
 
